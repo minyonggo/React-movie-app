06a5aea732ba9a744f66e93cad5ffe6f753af436
Minyong <ricky23@yonsei.ac.kr>
2020-06-10 16:34:40 +0900

    #3.1 class Components and State

--- src/App.js
+++ src/App.js
@@ -1,45 +1,28 @@
 import React from 'react';
 import Proptypes from 'prop-types';
 
-function Food({ name, image, rating }) {
-  return (
-    <div>
-      <h3>I Love {name}.</h3>
-      <h4>Rating {rating} / 5</h4>
-      <img src={image} alt={"image of " + name} />
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name : Proptypes.string.isRequired,
-  image : Proptypes.string.isRequired,
-  rating : Proptypes.number.isRequired 
-};
-
-const foodILike = [
-  {
-    id : 1,
-    name : "hamburger",
-    image : "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/01/a0001010/img/basic/a0001010_main.jpg?20200317173246&q=80&rw=750&rh=536",
-    rating : 5  
-  },
-  {
-    id : 2,
-    name : "donggas",
-    image : "https://image-cdn.hypb.st/https%3A%2F%2Fkr.hypebeast.com%2Ffiles%2F2019%2F09%2Ftonkatsu-mania-pork-cutlet-seoul-restaurant-top-1.jpg?q=75&w=800&cbr=1&fit=max",
-    rating : 4
+class App extends React.Component {
+  state = {
+    count : 0,
   }
-];
 
-const FoodList = foodILike.map(Element => <Food key={Element.id} name={Element.name} image={Element.image} rating={Element.rating} />);
+  Add = () => {
+    this.setState(current => ({count : current.count + 1}));
+  }
 
-function App() {
-  return (
-    <div>
-      {FoodList}
-    </div>
-  );
+  Minus = () => {
+    this.setState(current => ({count : current.count - 1}));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>The number is {this.state.count} </h1>
+        <button onClick={this.Add} >Add</button>
+        <button onClick={this.Minus} >Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
