039102357463d1aa6f8a9a78b02cb71da26c3af7
Minyong <ricky23@yonsei.ac.kr>
2020-06-10 21:21:44 +0900

    Updates

---
+++ precache-manifest.0cce352e68e737212d1ae8da66931789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f628154fe18cb80ef097d916ccba9df1",
+    "url": "/React-movie-app/index.html"
+  },
+  {
+    "revision": "813d4fbb3e1dab15048b",
+    "url": "/React-movie-app/static/css/main.33ce7a1d.chunk.css"
+  },
+  {
+    "revision": "78a8c6d4641937053d35",
+    "url": "/React-movie-app/static/js/2.19735fbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-movie-app/static/js/2.19735fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "813d4fbb3e1dab15048b",
+    "url": "/React-movie-app/static/js/main.996fb365.chunk.js"
+  },
+  {
+    "revision": "c0df2754d501a94db16c",
+    "url": "/React-movie-app/static/js/runtime-main.ad2062b6.js"
+  }
+]);
