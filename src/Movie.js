b80e084699b5cb7d2f312f33d5f9ad3c9704811e
Minyong <ricky23@yonsei.ac.kr>
2020-06-10 21:11:25 +0900

    #4.2 / #4.3 Styling the Movies & Adding Genres

--- src/Movie.js
+++ src/Movie.js
@@ -3,13 +3,18 @@ import PropTypes from 'prop-types';
 
 import './Movie.css';
 
-function Moive({ title, year, summary, poster }) {
+function Moive({ title, year, summary, poster, genres }) {
     
     return (
         <div className="movie">
             <h2 className="movie_title">{title}</h2>
             <h3 className="movie_year">{year}</h3>
-            <p className="movie_summary">{summary}</p>
+            <p className="movie_summary">{summary.slice(0,200)}...</p>
+            <ul className="genres">
+                {genres.map( (genre, index) => (
+                    <li key={index} className="genre">{genre}</li>
+                ))}
+            </ul>
             <img className="movie_poster" src={poster} alt={"poster image of"+title} />
         </div>
     );
@@ -20,6 +25,7 @@ Moive.propTypes = {
     year: PropTypes.number,
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 
 export default Moive;
